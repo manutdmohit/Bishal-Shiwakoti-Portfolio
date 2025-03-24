e9a60e9b34a5ee0f0a54c531477c8588c9aa9d60
Mohit Saud <mohit@prabidhilabs.com>
2025-03-24 12:36:42 +0545

    Remove footer from the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import About from '@/components/About';
 import Collaboration from '@/components/Collaboration';
 import Contact from '@/components/Contact';
-import Footer from '@/components/footer';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
@@ -14,7 +13,6 @@ export default function Home() {
       <Skills />
       <Projects />
       <Collaboration />
-      <Footer />
     </div>
   );
 }
